6360927fdfc5e055128f20f96e693435f4e03333
Mike Amundsen <mamund@yahoo.com>
2011-11-15 03:53:26 +0000

    modules

---
+++ app.js
@@ -0,0 +1,14 @@
+/* 2011-11-07 (mca) */
+
+var app = require('express').createServer();
+
+var g = {};
+g.port = (process.env.C9_PORT ? process.env.C9_PORT : 80);
+g.ip = '0.0.0.0'; 
+
+app.get('/', function(req, res){
+  var q = (req.query.q||'');
+  res.send('hello world<br />'+q);
+});
+
+app.listen(g.port, g.ip);
